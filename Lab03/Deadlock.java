784422b82c4111fa2175a30063a5de21d5f6a361
Arun Kollara <arunm201192@gmail.com>
2020-03-08 20:03:36 +0000

    Updated Deadlock for comments.

--- Lab03/Deadlock.java
+++ Lab03/Deadlock.java
@@ -1,14 +1,12 @@
 public class Deadlock {
-
     static class Friend {
         private final String name;
-
         public Friend(String name) { this.name = name; }
-
         public String getName() { return this.name; }
 
         public synchronized void bow(Friend bower) {
             System.out.format("%s: %s" + "  has bowed to me!%n", this.name, bower.getName());
+            /* Following line is vulnerable. Try commenting and then running the code. */
             bower.bowBack(this);
         }
 
